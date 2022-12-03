b47ccb134ff1fdb4dc80b167dd7217b0f8a56b11
Bartek <michasbartek@gmail.com>
2022-12-03 20:46:11 +0100

    mabey

--- pong.py
+++ pong.py
@@ -1,43 +1,91 @@
 import pygame
 pygame.init()
 
-FPS = 60
+FPS = 10
 WIDTH, HEIGHT = 700, 500
+PADDLE_WIDTH, PADDLE_HEIGHT = 50, 100
+
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PONG")
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+ALMOST_BLACK = (30, 30, 30)
 
 class Paddle:
-    COLORO = WHITE
+    COLOR = WHITE
+    VELOCITY = 50
+
+
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
-        self.widt = width
+        self.width = width
         self.height = height
 
+    
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+    
+    def move(self, up=True):
+        if up:
+            self.y -= self.VELOCITY
+        else:
+            self.y += self.VELOCITY
 
 
+def draw(win, paddles):
+    win.fill(BLACK)
+    for paddle in paddles:
+        paddle.draw(win)
+    
+    blockSize = 50
+    for x in range(0, WIDTH, blockSize):
+        for y in range(0, HEIGHT, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(win, ALMOST_BLACK, rect, 1)
+
+    pygame.display.update()
 
 
-def draw(win):
-    win.fill(WHITE)
+def handle_paddle_movement(keys, left_paddle, right_paddle):
+    if keys[pygame.K_w]:
+        left_paddle.move(up=True)
+
+    if keys[pygame.K_s]:
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_UP]:
+        right_paddle.move(up=True)
+
+    if keys[pygame.K_DOWN]:
+        right_paddle.move(up=False)
+
+def drawGrid():
+    
+
     pygame.display.update()
 
 
 def main():
     run = True
     clock = pygame.time.Clock()
-
+    left_paddle = Paddle(50, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 50 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
     while run:
-        clock(WINDOW)
+        clock.tick(FPS)
+        draw(WINDOW, [left_paddle, right_paddle])
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-    
+
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, left_paddle, right_paddle)
+
     pygame.quit()
 
-if __name__ == 'main':
+
+if __name__ == '__main__':
     main()
