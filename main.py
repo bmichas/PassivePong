a85df09a0eaeb5de069ec42832f514363da047ca
Bartek <michasbartek@gmail.com>
2022-12-07 22:35:15 +0100

    added second ai gen

--- main.py
+++ main.py
@@ -18,17 +18,25 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 env = Pong(WINDOW, WIDTH, HEIGHT, VELOCITY, WIN_SCORE)
 random_ai = RandomAi()
 simple_ai = SimpleAi()
+
 states, states_hash = env.get_all_states()
 print('State:', len(states))
-
 tree = env.get_tree(states)
 policy = env.get_policy(states)
 in_place_ai = InPlace(tree, env, policy)
 best_strategy_tree = in_place_ai.value_iteration(states, 0.9, 0.0001)
 print('done')
-
 env.set_left_ai(in_place_ai)
+
+states, states_hash = env.get_all_states()
+print('State:', len(states))
+tree = env.get_tree(states)
+policy = env.get_policy(states)
+in_place_ai = InPlace(tree, env, policy)
+best_strategy_tree = in_place_ai.value_iteration(states, 0.9, 0.0001)
+print('done')
 env.set_right_ai(in_place_ai)
+
 sim1 = Simulation(FPS, env, WIN_SCORE)
 for _ in range(EPOCH):
     sim1.run()
